336807bb7515a9ec66ab63330079fe43f92ac890
areum0116 <61903747+areum0116@users.noreply.github.com>
2024-11-29 10:44:25 +0900

    Time: 3 ms (86.81%), Space: 49.5 MB (46.74%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == '1') {
+                    DFSMarking(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void DFSMarking(char[][] grid, int i, int j) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1')
+            return;
+        grid[i][j] = '0';
+        DFSMarking(grid, i+1, j);
+        DFSMarking(grid, i-1, j);
+        DFSMarking(grid, i, j+1);
+        DFSMarking(grid, i, j-1);
+    }
+}
