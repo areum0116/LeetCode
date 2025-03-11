59fe8ad3ef12429ef8f4fe9728e400f146417b24
Areum Kim <areum0116@naver.com>
2025-03-11 16:36:38 +0900

    Time: 881 ms (31.61%), Space: 0B (100%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT MAX(NUM) NUM
+FROM (
+    SELECT NUM
+    FROM MYNUMBERS
+    GROUP BY NUM
+    HAVING COUNT(NUM)=1
+    ORDER BY NUM DESC
+    LIMIT 1
+) A
